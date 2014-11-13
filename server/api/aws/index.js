fd221172b4eed28769c766ba861b8b987cd37bea
Ilya Lovriakov <ilya.lovriakov@booking.com>
2014-11-13 09:28:14 +0100

    some rafactor

--- server/api/aws/index.js
+++ server/api/aws/index.js
@@ -1,3 +1,6 @@
+/*jslint indent: 2, nomen: true, node: true*/
+/*global process, require, console, exports, module, Buffer*/
+
 'use strict';
 
 var AWS = require('aws-sdk'),
@@ -8,8 +11,8 @@ var AWS = require('aws-sdk'),
 
 AWS.config.update(
   {
-    "accessKeyId": "",
-    "secretAccessKey": "",
+    "accessKeyId": process.env.AWS_KEY_ID,
+    "secretAccessKey": process.env.AWS_SECRET_KEY,
     "region": "us-west-2",
     "bucket": "month-reports"
   }
@@ -23,58 +26,52 @@ function handleError(res, err, line) {
 
 getExpiryTime = function () {
   var _date = new Date();
-  return '' + (_date.getFullYear()) + '-' + (_date.getMonth() + 1) + '-' +
+  return (_date.getFullYear()) + '-' + (_date.getMonth() + 1) + '-' +
     (_date.getDate() + 1) + 'T' + (_date.getHours() + 3) + ':' + '00:00.000Z';
 };
 
-createS3Policy = function(contentType, callback) {
-  var date = new Date();
-  var s3Policy = {
-    'expiration': getExpiryTime(),
-    'conditions': [
-      ['starts-with', '$key', 's3UploadExample/'],
-      {'bucket': "month-reports"},
-      {'acl': 'public-read'},
-      ['starts-with', '$Content-Type', contentType],
-      {'success_action_status' : '201'}
-    ]
-  };
-
-  // stringify and encode the policy
-  var stringPolicy = JSON.stringify(s3Policy);
-  var base64Policy = new Buffer(stringPolicy, 'utf-8').toString('base64');
-
-  // sign the base64 encoded policy
-  var signature = crypto.createHmac('sha1', "IaVwBB4Lds4foQ9SRSmaREtiZWOMl50yxcshb6xM")
-    .update(new Buffer(base64Policy, 'utf-8')).digest('base64');
-
-  // build the results object
-  var s3Credentials = {
-    s3Policy: base64Policy,
-    s3Signature: signature,
-    AWSAccessKeyId: "AKIAJAOJ5UR5U5ARJKNA"
-  };
+createS3Policy = function (contentType, callback) {
+  var date = new Date(),
+    s3Policy = {
+      'expiration': getExpiryTime(),
+      'conditions': [
+        ['starts-with', '$key', 's3UploadExample/'],
+        {'bucket': "month-reports"},
+        {'acl': 'public-read'},
+        ['starts-with', '$Content-Type', contentType],
+        {'success_action_status' : '201'}
+      ]
+    },
+    stringPolicy = JSON.stringify(s3Policy),
+    base64Policy = new Buffer(stringPolicy, 'utf-8').toString('base64'),
+    signature = crypto.createHmac('sha1', "IaVwBB4Lds4foQ9SRSmaREtiZWOMl50yxcshb6xM")
+      .update(new Buffer(base64Policy, 'utf-8')).digest('base64'),
+    s3Credentials = {
+      s3Policy: base64Policy,
+      s3Signature: signature,
+      AWSAccessKeyId: "AKIAJAOJ5UR5U5ARJKNA"
+    };
 
   // send it back
   callback(s3Credentials);
 };
 
-exports.getS3Policy = function(req, res) {
-    createS3Policy(req.query.mimeType, function (creds, err) {
-        if (!err) {
-            return res.send(200, creds);
-        } else {
-            return res.send(500, err);
-        }
-    });
+exports.getS3Policy = function (req, res) {
+  createS3Policy(req.query.mimeType, function (creds, err) {
+    if (!err) {
+      return res.send(200, creds);
+    } else {
+      return res.send(500, err);
+    }
+  });
 };
 
 exports.getClientConfig = function (req, res, next) {
-    return res.json(200, {
-        awsConfig: {
-            bucket: "month-reports"
-        }
-    });
+  return res.json(200, {
+    awsConfig: {
+      bucket: "month-reports"
+    }
+  });
 };
 
 exports.removeFile = function (key, callback) {
@@ -82,15 +79,18 @@ exports.removeFile = function (key, callback) {
     Bucket: "month-reports",
     Key: key
   };
-  s3.deleteObject(params, function(err, data) {
-    if (err) callback(err);
-    else callback(null, data);
+  s3.deleteObject(params, function (err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      callback(null, data);
+    }
   });
 };
 
 // call only like
 // aws.putStream.bind({image: image, res: res, buf: buf})
-exports.putStream = function(data) {
+exports.putStream = function () {
   // set stream length
 
   console.log('put file called');
@@ -98,22 +98,22 @@ exports.putStream = function(data) {
   // stdout.length = this.image.size;
   var nameArr = this.image.awsKey.split('/'),
     buf = this.buf,
+    data = {
+      Bucket: "month-reports",
+      Body: buf // error here
+    },
     name;
 
   name = nameArr[nameArr.length - 1];
-  name = Math.random()*9999 + '-crop-' + name;
+  name = Math.random() * 9999 + '-crop-' + name;
 
   console.log(name);
 
-  var data = {
-    Bucket: "month-reports",
-    Key: name,
-    Body: buf // error here
-  };
+  data.Key = name;
 
   // // res.send(200, data);
 
-  s3.putObject(data, function(err, data) {
+  s3.putObject(data, function (err, data) {
     // if (err) { handleError(res, err); }
     // res.send(200, data);
     console.log("done", data);
